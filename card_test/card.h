1df20b079fb2e5072efd44718240824b391dd1f9
xertyx <ethan197019@gmail.com>
2023-04-16 14:55:22 -0400

    [add] Card shuffling function

--- card_test/card.h
+++ card_test/card.h
@@ -1,6 +1,26 @@
 #ifndef _CARD_H
 #define _CARD_H
 
+#include <stdlib.h>
+#include <stdint.h>
+
+#define NUM_DECK 6
+#define NUM_CARD 52
+#define NUM_CARD_EACH_SUIT
+#define NUM_SUIT 4
+
+typedef enum {CLUB = 0, DIAMOND = 1, HEART = 2, SPADE = 3} Suit;
+
+typedef struct{
+	Suit type;
+	uint8_t value; // From 1 - 13, 9 will indicate noVal
+} Card;
+
+
+// This function assume that srand has been setup
+// This function assume user give a valid pointer
+void Card_Init(void *buf);
+
 
 
 #endif
